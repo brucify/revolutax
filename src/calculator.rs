29c9215f01768645d3e4697b930d77e8fb1cf511
Bruce Yinhe <bruce.yinhe@gmail.com>
2022-04-28 22:55:03 +0200

    find_cost WIP

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,6 +1,8 @@
 use std::io;
+use std::ops::Sub;
 use log::debug;
 use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use crate::transaction::{Currency, Transaction, TransactionType};
 
 
@@ -31,24 +33,24 @@ impl TaxableTransaction {
 
 #[derive(Debug)]
 struct CostBook {
+    base: Currency,
     currency: Currency,
     costs: Vec<Cost>,
 }
 
 impl CostBook {
     fn new(currency: Currency, base: Currency) -> CostBook {
-        let cash = Cost{
-            paid_amount: Default::default(),
-            exchanged: Cash::new(base, Default::default())
-        };
+        let cash = Cash::new(base.clone(), Default::default());
+        let cash_cost = Cost::new(Default::default(), cash);
         CostBook {
+            base,
             currency,
-            costs: vec![cash],
+            costs: vec![cash_cost],
         }
     }
 
-    fn add_buy(&mut self, t: &Transaction, base: &Currency) {
-        if t.exchanged_currency.eq(base) {
+    fn add_buy(&mut self, t: &Transaction) {
+        if t.exchanged_currency.eq(&self.base) {
             self.find_cash_mut().map(|cost| {
                 if let Money::Cash(cash) = &mut cost.exchanged {
                     cash.amount += t.exchanged_amount;
@@ -56,26 +58,31 @@ impl CostBook {
                 }
             });
         } else {
-            let coupon = Cost{
-                paid_amount: t.paid_amount,
-                exchanged: Coupon::new(t.exchanged_currency.clone(), t.exchanged_amount, t.date.clone())
-            };
-            self.costs.push(coupon);
+            let coupon = Coupon::new(t.exchanged_currency.clone(), t.exchanged_amount, t.date.clone());
+            let coupon_cost = Cost::new(t.paid_amount, coupon);
+            self.costs.push(coupon_cost);
         }
     }
 
     // TODO
-    fn add_sell(&mut self, t: &Transaction, base: &Currency) {
-        if t.exchanged_currency.eq(base) {
+    fn add_sell(&mut self, transaction: &Transaction) {
+        let income = transaction.to_money(&self.base);
+        debug!("income: {:?}", income);
+        let cost = self.find_cost(&income, transaction.paid_amount);
+        debug!("cost: {:?}", cost);
+        // TODO deduct found cost from self
+
+        if transaction.exchanged_currency.eq(&self.base) {
             self.find_cash_mut().map(|cost| {
-                if cost.paid_amount >= t.paid_amount {
+                if cost.paid_amount >= transaction.paid_amount {
                     // TODO tax report
-                    let x = cost.deduct(&t.paid_amount)
+                    let x = cost.deduct(&transaction.paid_amount)
                         .map(|cost| {
                             TaxableTransaction{
-                                currency: t.paid_currency.clone(),
-                                amount: t.paid_amount,
-                                income: Cash::new(t.exchanged_currency.clone(), t.exchanged_amount),
+                                currency: transaction.paid_currency.clone(),
+                                amount: transaction.paid_amount,
+                                // income: transaction.to_money(&self.base),
+                                income: Cash::new(transaction.exchanged_currency.clone(), transaction.exchanged_amount),
                                 cost: vec![cost],
                                 net_income: None
                             }
@@ -86,9 +93,10 @@ impl CostBook {
                     let x = cost.deduct(&cost.paid_amount.clone())
                         .map(|cost| {
                             TaxableTransaction{
-                                currency: t.paid_currency.clone(),
-                                amount: t.paid_amount,
-                                income: Cash::new(t.exchanged_currency.clone(), t.exchanged_amount),
+                                currency: transaction.paid_currency.clone(),
+                                amount: transaction.paid_amount,
+                                // income: transaction.to_money(&self.base),
+                                income: Cash::new(transaction.exchanged_currency.clone(), transaction.exchanged_amount),
                                 cost: vec![cost],
                                 net_income: None
                             }
@@ -97,7 +105,7 @@ impl CostBook {
                 }
             });
         } else {
-            let x = Coupon::new(t.exchanged_currency.clone(), t.exchanged_amount, t.date.clone());
+            let x = Coupon::new(transaction.exchanged_currency.clone(), transaction.exchanged_amount, transaction.date.clone());
         }
     }
 
@@ -109,6 +117,71 @@ impl CostBook {
             }
         })
     }
+
+    fn find_cost(&self, income: &Money, paid_amount: Decimal) -> Option<Vec<Cost>> {
+        match income {
+            Money::Cash(_) => {
+                let (remaining, costs) = self.find_cash_costs(paid_amount, vec![]);
+                let (remaining, costs) = self.find_coupon_costs(remaining, costs);
+                Some(costs)
+            }
+            Money::Coupon(_) => {
+                let (remaining, costs) = self.find_coupon_costs(paid_amount, vec![]);
+                let (remaining, costs) = self.find_cash_costs(remaining, costs);
+                Some(costs)
+            }
+        }
+    }
+
+    fn find_coupon_costs(&self, remaining: Decimal, costs: Vec<Cost>) -> (Decimal, Vec<Cost>) {
+        self.costs.iter()
+            .fold((remaining, costs), |(remaining, mut acc), cost| {
+                if remaining.eq(&dec!(0)) {
+                    (remaining, acc)
+                } else {
+                    let amount = remaining.min(cost.paid_amount);
+                    match &cost.exchanged {
+                        Money::Cash(_) => (remaining, acc),
+                        Money::Coupon(coupon) => {
+                            let coupon = Coupon::new(self.base.clone(), coupon.amount / cost.paid_amount * amount, coupon.date.clone());
+                            let coupon_cost = Cost::new(remaining, coupon);
+                            acc.push(coupon_cost);
+                            (remaining.sub(amount), acc)
+                        }
+                    }
+                }
+            })
+    }
+
+    fn find_cash_costs(&self, paid_amount: Decimal, costs: Vec<Cost>) -> (Decimal, Vec<Cost>) {
+        self.costs.iter()
+            .fold((paid_amount, costs), |(remaining, mut acc), cost| {
+                if remaining.eq(&dec!(0)) {
+                    (remaining, acc)
+                } else {
+                    let amount = remaining.min(cost.paid_amount);
+                    match &cost.exchanged {
+                        Money::Coupon(_) => (remaining, acc),
+                        Money::Cash(cash) => {
+                            let cash = Cash::new(self.base.clone(), cash.amount / cost.paid_amount * amount);
+                            let cash_cost = Cost::new(remaining, cash);
+                            acc.push(cash_cost);
+                            (remaining.sub(amount), acc)
+                        }
+                    }
+                }
+            })
+    }
+}
+
+impl Transaction {
+    fn to_money(&self, base: &Currency) -> Money {
+        if self.exchanged_currency.eq(base) {
+            Cash::new(self.exchanged_currency.clone(), self.exchanged_amount)
+        } else {
+            Coupon::new(self.exchanged_currency.clone(), self.exchanged_amount, self.date.clone())
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -118,6 +191,10 @@ struct Cost {
 }
 
 impl Cost {
+    fn new(paid_amount: Decimal, exchanged: Money) -> Cost {
+        Cost{ paid_amount, exchanged }
+    }
+
     fn deduct(&mut self, paid_amount: &Decimal) -> Option<Money> {
         if let Money::Cash(c) = &mut self.exchanged {
             let deducted = Cash::new(c.currency.clone(), c.amount * paid_amount / self.paid_amount);
@@ -159,8 +236,8 @@ pub(crate) async fn tax(txns: &Vec<Transaction>, currency: &Currency, base: &Cur
         txns.iter().fold((vec![], CostBook::new(currency.clone(), base.clone())),
                          |(acc, mut book), t| {
             match t.r#type {
-                TransactionType::Buy => book.add_buy(t, base),
-                TransactionType::Sell => book.add_sell(t, base),
+                TransactionType::Buy => book.add_buy(t),
+                TransactionType::Sell => book.add_sell(t),
             }
             (acc, book)
         });
