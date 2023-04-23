dae470d2db40e3ee4d00cba7985a79ebdb40f931
Bruce Yinhe <bruce.yinhe@gmail.com>
2023-04-23 15:02:06 +0200

    Unnecessary block_on

--- src/cryptotax.rs
+++ src/cryptotax.rs
@@ -1,18 +1,17 @@
 use crate::reader::row::to_trades;
 use crate::{calculator, reader, writer};
-use futures::executor::block_on;
 use log::info;
 use std::path::PathBuf;
 
 /// Reads the transactions with type `Exchange` from the path and prints the results to
 /// `std::io::stdout()`.
-pub fn print_exchanges(path: &PathBuf) -> std::io::Result<()> {
+pub async fn print_exchanges(path: &PathBuf) -> std::io::Result<()> {
     let now = std::time::Instant::now();
-    let rows = block_on(reader::read_exchanges(path))?;
+    let rows = reader::read_exchanges(path).await?;
     info!("Done reading csv file. Elapsed: {:.2?}", now.elapsed());
 
     let now = std::time::Instant::now();
-    block_on(writer::print(&rows))?;
+    writer::print(&rows).await?;
     info!("Done printing rows. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
@@ -21,13 +20,13 @@ pub fn print_exchanges(path: &PathBuf) -> std::io::Result<()> {
 /// Reads the transactions with type `Exchange` from the path,
 /// filters for the target currency,
 /// and finally prints the results to `std::io::stdout()`.
-pub fn print_exchanges_in_currency(path: &PathBuf, currency: &String) -> std::io::Result<()> {
+pub async fn print_exchanges_in_currency(path: &PathBuf, currency: &String) -> std::io::Result<()> {
     let now = std::time::Instant::now();
-    let rows = block_on(reader::read_exchanges_in_currency(path, currency))?;
+    let rows = reader::read_exchanges_in_currency(path, currency).await?;
     info!("Done reading csv file. Elapsed: {:.2?}", now.elapsed());
 
     let now = std::time::Instant::now();
-    block_on(writer::print(&rows))?;
+    writer::print(&rows).await?;
     info!("Done printing rows. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
@@ -37,17 +36,17 @@ pub fn print_exchanges_in_currency(path: &PathBuf, currency: &String) -> std::io
 /// filters for the target currency,
 /// converts the csv rows into transactions,
 /// and finally prints the results to `std::io::stdout()`.
-pub fn merge_exchanges(path: &PathBuf, currency: &String) -> std::io::Result<()> {
+pub async fn merge_exchanges(path: &PathBuf, currency: &String) -> std::io::Result<()> {
     let now = std::time::Instant::now();
-    let rows = block_on(reader::read_exchanges_in_currency(path, currency))?;
+    let rows = reader::read_exchanges_in_currency(path, currency).await?;
     info!("reader::read_exchanges_in_currency done. Elapsed: {:.2?}", now.elapsed());
 
     let now = std::time::Instant::now();
-    let trades =  block_on(to_trades(&rows, currency))?;
+    let trades =  to_trades(&rows, currency).await?;
     info!("reader::to_transactions done. Elapsed: {:.2?}", now.elapsed());
 
     let now = std::time::Instant::now();
-    block_on(writer::print(&trades))?;
+    writer::print(&trades).await?;
     info!("calculator::tax done. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
@@ -58,21 +57,21 @@ pub fn merge_exchanges(path: &PathBuf, currency: &String) -> std::io::Result<()>
 /// converts the csv rows into transactions,
 /// calculates tax from the transactions,
 /// and finally prints the results to `std::io::stdout()`.
-pub fn calculate_tax(path: &PathBuf, currency: &String, base: &String) -> std::io::Result<()> {
+pub async fn calculate_tax(path: &PathBuf, currency: &String, base: &String) -> std::io::Result<()> {
     let now = std::time::Instant::now();
-    let rows = block_on(reader::read_exchanges_in_currency(path, currency))?;
+    let rows = reader::read_exchanges_in_currency(path, currency).await?;
     info!("Done reading csv file. Elapsed: {:.2?}", now.elapsed());
 
     let now = std::time::Instant::now();
-    let trades =  block_on(to_trades(&rows, currency))?;
+    let trades = to_trades(&rows, currency).await?;
     info!("Done converting to transactions. Elapsed: {:.2?}", now.elapsed());
 
     let now = std::time::Instant::now();
-    let trades =  block_on(calculator::tax(&trades, currency, base))?;
+    let trades = calculator::tax(&trades, currency, base).await?;
     info!("Done calculating taxes. Elapsed: {:.2?}", now.elapsed());
 
     let now = std::time::Instant::now();
-    block_on(writer::print(&trades))?;
+    writer::print(&trades).await?;
     info!("Done printing rows. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
