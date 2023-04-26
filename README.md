b278f8ae68c591541caef79d21856508a801f45e
Bruce Yinhe <bruce.yinhe@gmail.com>
2023-04-26 13:04:41 +0200

    README, LICENSE, readability

--- README.md
+++ README.md
@@ -30,20 +30,18 @@ Here is an example input CSV file `account_statement.csv`:
 
 ```csv
 Type,Product,Started Date,Completed Date,Description,Amount,Currency,Fiat amount,Fiat amount (inc. fees),Fee,Base currency,State,Balance
-EXCHANGE,Current,2023-01-01 10:00:00,2023-01-01 10:00:00,Exchanged to EOS,30.0000,EOS,600.00,609.15,9.15,SEK,COMPLETED,30.0000
-EXCHANGE,Current,2023-01-02 10:00:00,2023-01-02 10:00:00,Exchanged to SEK,-30.0000,EOS,-400.00,-394.86,5.14,SEK,COMPLETED,0.0000
-TRANSFER,Current,2023-02-08 10:00:00,2023-02-08 10:00:00,Transferred to Savings,-10.0000,EOS,-200.00,-200.00,0.00,SEK,COMPLETED,40.0000
-TRANSFER,Current,2023-04-04 10:00:00,2023-04-04 10:00:00,Transferred to Current,100.0000,EOS,2000.00,2000.00,0.00,SEK,COMPLETED,140.0000
-CARD_PAYMENT,Current,2023-05-06 10:00:00,2023-05-06 10:00:00,Payment to Amazon,-25.0000,EOS,-500.00,-495.75,4.25,SEK,COMPLETED,65.0000
+EXCHANGE,Current,2023-01-01 10:00:00,2023-01-01 10:00:00,Exchanged to EOS,100.0000,EOS,600.00,609.15,9.15,SEK,COMPLETED,100.0000
+EXCHANGE,Savings,2023-01-01 11:00:00,2023-01-01 11:00:00,Exchanged to EOS,50.0000,EOS,300.00,304.15,4.15,SEK,COMPLETED,50.0000
+EXCHANGE,Current,2023-01-02 10:00:00,2023-01-02 10:00:00,Exchanged to SEK,-30.0000,EOS,-400.00,-394.86,5.14,SEK,COMPLETED,70.0000
+CARD_PAYMENT,Current,2023-05-06 10:00:00,2023-05-06 10:00:00,Payment to Amazon,-25.0000,EOS,-500.00,-495.75,4.25,SEK,COMPLETED,45.0000
 ```
 
 The program reads the input CSV file and outputs the following tax report `tax.csv`:
 
 ```csv
 Date;Currency;Amount;Income;Cost;Net Income
-2023-01-02 10:00:00;EOS;-30;394.86;-609.1500;-214.2900
-2023-04-04 11:00:00;EOS;-50;594.86;-1009.6500;-414.7900
-2023-05-06 10:00:00;EOS;-25;495.75;-505.7200;-9.9700
+2023-01-02 10:00:00;EOS;-30;394.86;-182.7450;212.1150
+2023-05-06 10:00:00;EOS;-25;495.75;-152.2875;343.4625
 ```
 This report shows the following information for each transaction:
 
@@ -53,3 +51,18 @@ This report shows the following information for each transaction:
 * Income (`Försäljningspris`): The income generated from the transaction, calculated in the base currency (SEK).
 * Cost (`Omkostnadsbelopp`): The cost of the transaction, calculated in the base currency (SEK).
 * Net Income (`Vinst/förlust`): The net income generated from the transaction, calculated by subtracting the cost from the income.
+  
+#### Current vs. Savings
+
+The program algorithm  takes into account the two types of `Product` of transactions: `Savings` and `Current`.
+Specifically, when calculating the tax for a trade where you sold cryptocurrency, the program will first try to
+find the costs for the sold crypto in the `Current` transactions and deduct them from there. Only when there are
+not enough available costs to deduct on the `Current` transactions will the program deduct from the `Savings`
+transactions.
+
+## License
+```
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use log::info;
+use reader::RevolutRow2022;
+use reader::RevolutRow2023;
 use std::io::Result;
 use std::path::PathBuf;
+use std::time::Instant;
 
 mod calculator;
 mod reader;
@@ -9,12 +12,12 @@ mod writer;
 /// Reads the transactions with type `Exchange` from the path and prints the results to
 /// `std::io::stdout()`.
 pub async fn print_exchanges(path: &PathBuf) -> Result<()> {
-    let now = std::time::Instant::now();
-    let rows = reader::RevolutRow2022::read_exchanges(path).await?;
+    let now = Instant::now();
+    let rows = RevolutRow2022::read_exchanges(path).await?;
     info!("Done reading csv file. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
-    writer::print(&rows).await?;
+    let now = Instant::now();
+    writer::print_csv_rows(&rows).await?;
     info!("Done printing rows. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
@@ -24,12 +27,12 @@ pub async fn print_exchanges(path: &PathBuf) -> Result<()> {
 /// filters for the target currency,
 /// and finally prints the results to `std::io::stdout()`.
 pub async fn print_exchanges_in_currency(path: &PathBuf, currency: &String) -> Result<()> {
-    let now = std::time::Instant::now();
-    let rows = reader::RevolutRow2022::read_exchanges_in_currency(path, currency).await?;
+    let now = Instant::now();
+    let rows = RevolutRow2022::read_exchanges_in_currency(path, currency).await?;
     info!("Done reading csv file. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
-    writer::print(&rows).await?;
+    let now = Instant::now();
+    writer::print_csv_rows(&rows).await?;
     info!("Done printing rows. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
@@ -40,16 +43,16 @@ pub async fn print_exchanges_in_currency(path: &PathBuf, currency: &String) -> R
 /// converts the csv rows into transactions,
 /// and finally prints the results to `std::io::stdout()`.
 pub async fn merge_exchanges(path: &PathBuf, currency: &String) -> Result<()> {
-    let now = std::time::Instant::now();
-    let rows = reader::RevolutRow2022::read_exchanges_in_currency(path, currency).await?;
-    info!("reader::RevolutRow2022::read_exchanges_in_currency done. Elapsed: {:.2?}", now.elapsed());
+    let now = Instant::now();
+    let rows = RevolutRow2022::read_exchanges_in_currency(path, currency).await?;
+    info!("RevolutRow2022::read_exchanges_in_currency done. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
-    let trades =  reader::RevolutRow2022::rows_to_trades(&rows, currency).await?;
+    let now = Instant::now();
+    let trades =  RevolutRow2022::rows_to_trades(&rows, currency).await?;
     info!("reader::to_transactions done. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
-    writer::print(&trades).await?;
+    let now = Instant::now();
+    writer::print_csv_rows(&trades).await?;
     info!("calculator::tax done. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
@@ -61,38 +64,38 @@ pub async fn merge_exchanges(path: &PathBuf, currency: &String) -> Result<()> {
 /// calculates tax from the transactions,
 /// and finally prints the results to `std::io::stdout()`.
 pub async fn calculate_tax_2022(path: &PathBuf, currency: &String, base_currency: &String) -> Result<()> {
-    let now = std::time::Instant::now();
-    let rows = reader::RevolutRow2022::read_exchanges_in_currency(path, currency).await?;
+    let now = Instant::now();
+    let rows = RevolutRow2022::read_exchanges_in_currency(path, currency).await?;
     info!("Done reading csv file. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
-    let trades = reader::RevolutRow2022::rows_to_trades(&rows, currency).await?;
+    let now = Instant::now();
+    let trades = RevolutRow2022::rows_to_trades(&rows, currency).await?;
     info!("Done converting to transactions. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
+    let now = Instant::now();
     let taxable_trades = calculator::taxable_trades(&trades, currency, base_currency).await?;
     info!("Done calculating taxes. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
-    writer::print(&taxable_trades).await?;
+    let now = Instant::now();
+    writer::print_csv_rows(&taxable_trades).await?;
     info!("Done printing rows. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
 }
 
 pub async fn calculate_tax_2023(path: &PathBuf) -> Result<()> {
-    let now = std::time::Instant::now();
-    let trades = reader::RevolutRow2023::deserialize_from(path).await?;
+    let now = Instant::now();
+    let trades = RevolutRow2023::deserialize_from(path).await?;
     info!("Done reading csv file. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
+    let now = Instant::now();
     let currency = trades.first().map(|t| t.paid_currency.clone()).unwrap();
     let base_currency = trades.first().map(|t| t.exchanged_currency.clone()).unwrap();
     let taxable_trades = calculator::taxable_trades(&trades, &currency, &base_currency).await?;
     info!("Done calculating taxes. Elapsed: {:.2?}", now.elapsed());
 
-    let now = std::time::Instant::now();
-    writer::print(&taxable_trades).await?;
+    let now = Instant::now();
+    writer::print_csv_rows(&taxable_trades).await?;
     info!("Done printing rows. Elapsed: {:.2?}", now.elapsed());
 
     Ok(())
